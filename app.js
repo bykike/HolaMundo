f657a5251fa20e2b764bb1cefd02c93ccca63b3e
Kike Algarra <kike@areavisual.net>
2018-11-23 19:34:14 +0100

    Hello World React with CDN
    Primera app de "Hola Mundo" con React usando CDN.

---
+++ app.js
@@ -0,0 +1,13 @@
+var HelloWorld = React.createClass({    
+  render: function() {    
+    return (    
+      <h1>Hello World CDN</h1>    
+    )    
+  }    
+});    
+    
+
+ReactDOM.render(    
+  <HelloWorld/>,    
+  document.getElementById('root')    
+);
